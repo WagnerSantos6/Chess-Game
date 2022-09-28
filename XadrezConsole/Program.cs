b15aefe14183f2b205de3c2ce31412b203779b65
Wagner Santos <wagnerstss@gmail.com>
2022-09-28 13:46:26 -0300

    Criado classe posição

--- XadrezConsole/Program.cs
+++ XadrezConsole/Program.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 
+using Tabuleiro;
+
 namespace XadrezConsole
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Posicao P;
+            P = new Posicao(3, 4);
+            Console.WriteLine("Posição: " + P);
         }
     }
 }

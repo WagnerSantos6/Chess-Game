48de943c048f0d51b66070595c15daa61faed69d
Wagner Santos <wagnerstss@gmail.com>
2022-09-29 12:53:41 -0300

    Tratando erros com excesao personalizada

---
+++ XadrezConsole/Tabuleiro/TabuleiroException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace Tabuleiro
+{
+    class TabuleiroException : Exception
+    {
+        public TabuleiroException(string msg) : base(msg)
+        {
+
+        }
+    }
+}
